734075cdedb988208f49bba43c23aabda6608211
unknown <vunguyenminhduc1410@gmail.com>
2024-09-28 19:51:20 +0700

    Hoan Thanh Cau 2a

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Array from './screens/Array';
 import Count from './screens/Count';
+import Cau2a from './screens/Cau2a';
 
 export default function App() {
   return (
     // <Array />
-    <Count />
+    // <Count />
+    <Cau2a />
   );
 }
 
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+export const images = {
+    Lock: require('../assets/lock.png'),
+    Eye: require('../assets/eye.png'),
+    AvatarUser: require('../assets/avatar_user.png'),
+};
