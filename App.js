0472e976050fb02f234ace25bfa3eb525974147f
unknown <vunguyenminhduc1410@gmail.com>
2024-09-28 19:44:03 +0700

    Hoan Thanh Count with useState hook

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Array from './screens/Array';
+import Count from './screens/Count';
 
 export default function App() {
   return (
-    <Array />
+    // <Array />
+    <Count />
   );
 }
 
